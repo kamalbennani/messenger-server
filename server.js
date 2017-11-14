b1ddabce1715ce0ec07bf4bb96dc9fc216e3021c
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2017-11-14 13:24:12 +0100

    comment code

--- server.js
+++ server.js
@@ -14,29 +14,36 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 
+// INTERESTING PART
+
 // POST Request sent by twilio
-app.post('/', (req, res) => {
-  const { Body, From } = req.body;
-  const message = {
-    body: Body,
-    from: From,
-    date: new Date().toString()
-  };
-
-  io.emit('new received message', message);
-});
-
-// Socket Configuration
-io.on('connection', function(socket) {
-  socket.on('new message', function(data) {
-    console.log('message received', data);
-    socket.broadcast.emit('new received message', {
-      body: data.body,
-      from: socket.id.slice(8),
-      date: new Date().toString()
+{
+  /*
+    app.post('/', (req, res) => {
+      const { Body, From } = req.body;
+      const message = {
+        body: Body,
+        from: From,
+        date: new Date().toString()
+      };
+    
+      io.emit('new received message', message);
     });
-  });
-});
+    
+    // Socket Configuration
+    io.on('connection', function(socket) {
+      socket.on('new message', function(data) {
+        console.log('message received', data);
+        socket.broadcast.emit('new received message', {
+          body: data.body,
+          from: socket.id.slice(8),
+          date: new Date().toString()
+        });
+      });
+    });
+  */
+}
 
+// Server Starts
 server.listen(3030);
 console.log('Server listen on port 3030');
